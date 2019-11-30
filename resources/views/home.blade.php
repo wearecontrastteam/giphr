d8c6cd6d402e0d1a671c4bb9b4f75ef9c2c8430a
mikeaag <mike.gatward@xenonsoft.co.uk>
2019-11-30 19:29:49 +0000

    Create initial ui (#3)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,76 +2,11 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-
-                <div class="card-body">
-                    <h2>Post a giph:</h2>
-                    <form method="POST" action="{{ route('post') }}">
-                        @csrf
-                        <div class="form-group row">
-                            <label for="name" class="col-md-2 col-form-label text-md-right">{{ __('Giphy ID') }}</label>
-
-                            <div class="col-md-8">
-                                <input id="giphy_id" type="text" class="form-control" name="giphy_id" required>
-                            </div>
-
-                            <div class="col-md-2">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Giph') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-
-                <div class="card-body">
-                    <h2>All giphs:</h2>
-                    @foreach($giphs as $giph)
-                        <div>
-                            [{{$giph->id}}] Giphy ID: {{$giph->giphy_id}}<br>
-                            User: (name: {{$giph->user->name}})<br>
-                            Likes:<br>
-                            @foreach($giph->likes as $like)
-                                by {{$like->user->name}} ({{$like->giphy_id}})<br>
-                            @endforeach
-                        </div>
-                        <form method="POST" action="{{ route('like', ['giph' => $giph->id]) }}">
-                            @csrf
-                            <div class="col-md-2">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('👍') }}
-                                </button>
-                            </div>
-                        </form>
-                        <br>
-                    @endforeach
-                </div>
-
-                <div class="card-body">
-                    <h2>All users:</h2>
-                    @foreach($users as $user)
-                        <div>
-                            Name: {{$user->name}}<br>
-                            Email: {{$user->email}}<br>
-                            Avatar: {{$user->avatar_giphy_id}}<br>
-                        </div>
-                        <br>
-                    @endforeach
-                </div>
-            </div>
+    <div class="row">
+        @include('partials.nav')
+        <div class="col-md-9">
+            <new-giph></new-giph>
+            <giph-timeline></giph-timeline>
         </div>
     </div>
 </div>
--- routes/api.php
+++ routes/api.php
@@ -13,18 +13,21 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+Route::middleware('auth')->namespace('Api')->group(function (){
+    Route::prefix('users')->name('users.')->group(function() {
+        Route::get('/', 'UserController@index')->name('index');
+        Route::get('/{user}', 'UserController@show')->name('show');
+    });
+
+    Route::prefix('giphs')->name('giphs.')->group(function() {
+        Route::get('/', 'GiphController@index')->name('index');
+        Route::get('/{giph}', 'GiphController@show')->name('show');
+        Route::post('/', 'GiphController@postGiph')->name('postGiph'); // rename to post or something else?
+        Route::post('/{giph}/likes', 'GiphController@like')->name('like');
+    });
 });
 
-Route::prefix('users')->name('users.')->group(function() {
-    Route::get('/', 'UserController@index')->name('index');
-    Route::get('/{user}', 'UserController@show')->name('show');
-});
-
-Route::prefix('giphs')->name('giphs.')->group(function() {
-    Route::get('/', 'GiphController@index')->name('index');
-    Route::get('/{giph}', 'GiphController@show')->name('show');
-    Route::post('/', 'GiphController@postGiph')->name('postGiph'); // rename to post or something else?
-    Route::post('/{giph}/likes', 'GiphController@like')->name('like');
-});
