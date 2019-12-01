d7b8e4a2a8965d4657621241832260c548d63fe1
Mike Gatward <mike.gatward@xenonsoft.co.uk>
2019-12-01 11:06:20 +0000

    wip

--- resources/js/components/Giph.vue
+++ resources/js/components/Giph.vue
@@ -2,19 +2,26 @@
     <div class="col-12 mb-2">
         <div class="card">
             <div class="card-body">
-                <img :src="profileUrl" class="rounded-circle profile mr-4">
-
-                <img :src="giphUrl">
-
-                <div class="row mt-2 ml-4">
-                    <button type="submit" class="btn btn-default" @click.prevent="like">
-                        <i class="fa fa-thumbs-o-up"></i>
-                    </button>
-                    <a href="btn btn-outline-default" @click.prevent="showLikes = true">{{ getLikeCount }}</a>
+                <div class="row">
+                    <div class="col-1">
+                        <img :src="profileUrl" class="rounded-circle profile">
+                    </div>
+                    <div class="col">
+                        <img :src="giphUrl">
+                    </div>
                 </div>
+            </div>
 
-                <div class="row mt-2 ml-4">
-                    <img v-for="like in giph.likes" :key="like.id" :src="getGiphyUrl(like.giphy_id)" style="height: 50px; margin-bottom: 10px;">
+            <div class="card-footer">
+                <div class="row">
+                    <div class="col-1 text-center">
+                        <a class="btn btn-outline-secondary" href="#" @click.prevent="like">
+                            {{ getLikeCount }} <i class="fa fa-thumbs-o-up"></i>
+                        </a>
+                    </div>
+                    <div class="col">
+                        <img v-for="like in giph.likes" :key="like.id" :src="getGiphyUrl(like.giphy_id)" style="height: 50px; margin-bottom: 5px; margin-right: 5px;">
+                    </div>
                 </div>
             </div>
         </div>
@@ -27,7 +34,7 @@
         props: ['giph'],
         data() {
             return {
-                showLikes: false
+
             };
         },
         computed: {
@@ -39,7 +46,7 @@
             },
             getLikeCount(){
                 return this.giph.likes_count || 0;
-            }
+            },
         },
         methods: {
             like(){
